dace013aecd04e607be34a7a0ed4384ecf65d448
derrickkimtai <derrickmoio92@gmail.com>
2023-08-11 12:22:53 +0300

    modules

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pytho3
+import hidden_4
+import types
+def print_names():
+    names = [name for name in dir(hidden_4) if not name.startswith('__')]
+    name.sort()
+    for name in names:
+        print(name)
+        if __name__ == '__main__';
+        print_names()
