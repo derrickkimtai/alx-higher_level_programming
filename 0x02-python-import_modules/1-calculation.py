9cc3b4fea5c03196cdbe84b2c27a7b1d0a6654af
derrickkimtai <derrickmoio92@gmail.com>
2023-08-11 09:33:19 +0300

    modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-import calculator_1 as calc
-a = 10
-b = 5
-sum_result = calc.add(a, b)
-diff_result = calc.sub(a, b)
-product_result = calc.mul(a, b)
-div_result = calc.div(a, b)
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-print("{:d} + {:d} = {:d}".format(a, b, sum_result))
-print("{:d} - {:d} = {:d}".format(a, b, diff_result))
-print("{:d} * {:d} = {:d}".format(a, b, product_result))
-print("{:d} / {:d} = {:d}".format(a, b, div_result))
+    a = 10
+    b = 5
+
+    sum_result = add(a, b)
+    diff_result = sub(a, b)
+    product_result = mul(a, b)
+    div_result = div(a, b)
+
+    print("{:d} + {:d} = {:d}".format(a, b, sum_result))
+    print("{:d} - {:d} = {:d}".format(a, b, diff_result))
+    print("{:d} * {:d} = {:d}".format(a, b, product_result))
+    print("{:d} / {:d} = {:d}".format(a, b, div_result))
