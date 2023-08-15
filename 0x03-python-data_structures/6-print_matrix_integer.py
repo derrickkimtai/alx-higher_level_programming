71cde1b46720a4e6b16765324557f9ead43830b1
derrickkimtai <derrickmoio92@gmail.com>
2023-08-15 12:43:32 +0300

    structures

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+            print(" ".join("{:d}".format(i)for i in row))
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return(None)
+    else:
+        max_num = my_list[0]
+        for num in my_list:
+            if num > max_num:
+                max_num = num
+        return max_num
