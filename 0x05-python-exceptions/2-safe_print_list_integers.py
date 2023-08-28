a96d77c8c9fde44cadab067e797b49204bca6045
derrickkimtai <derrickmoio92@gmail.com>
2023-08-28 15:22:52 +0300

    error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
                 if count < x:
                     print("{:d}".format(my_list[i]), end='')
                     count += 1
+                    if count == x:
+                        break
         print()
         return count
     except Exception:
