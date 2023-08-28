a576ecc00ca65493aecf1b060114bf42d4307b40
derrickkimtai <derrickmoio92@gmail.com>
2023-08-28 14:56:25 +0300

    errors

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pyhton3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) == int:
+                if count < x:
+                    print("{:d}".format(my_list[i]), end='')
+                    count += 1
+        print()
+        return count
+    except Exception:
+        return count
+
