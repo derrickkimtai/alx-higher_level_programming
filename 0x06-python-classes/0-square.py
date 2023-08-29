5c0100d1863f69468e69f2e7241921f7b98cf0aa
derrickkimtai <derrickmoio92@gmail.com>
2023-08-30 00:07:45 +0300

    clasess

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+"""
+This module defines the square class.
+
+This module contains the definition of the square class.
+"""
+
+
 class Square:
+    """
+    This class representts a square.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
     pass
