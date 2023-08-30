e5d063970d94528149bc8170f7771a58e8b87397
derrickkimtai <derrickmoio92@gmail.com>
2023-08-30 10:06:31 +0300

    clasess

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines the square class.
+
+This module contains the definition of the square class.
+"""
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
