4731274fcd297f1331ec747a56da4dd2e6cfcec8
derrickkimtai <derrickmoio92@gmail.com>
2023-08-30 10:14:19 +0300

    classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,6 +5,23 @@ This module defines the square class.
 This module contains the definition of the square class.
 """
 
+
 class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+            __size (int): The size of the square's sides.
+    Methods:
+            __init__(self, size):Intializes a new square instance.
+            area : calculates the area of the square.
+    """
     def __init__(self, size):
+        """
+        calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+
+        """
         self.__size = size
