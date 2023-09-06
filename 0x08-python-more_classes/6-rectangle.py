ba595e5bb8af13ca3485ac231d565e6db2580b9b
derrickkimtai <derrickmoio92@gmail.com>
2023-09-06 21:31:58 +0300

    more objects

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -16,6 +16,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
