ab982e80c69ca9540bc76802910f877938d5d5e8
derrickkimtai <derrickmoio92@gmail.com>
2023-09-08 21:16:24 +0300

    hash tables

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    if not all(isinstance(row, list) and all(isinstance(val, (int, float))
+        for val in row) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of intagers/floats")
+    if len(set(len(row) for row in matrix )) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    result = []
+    for row in matrix:
+        new_row = [round(val / div, 2)for val in row]
+        result.append(new_row)
+
+    return result
