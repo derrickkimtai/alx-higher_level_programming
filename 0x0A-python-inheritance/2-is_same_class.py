7b385459f61e62900a8a345dfe0ece4d170e8de8
derrickkimtai <derrickmoio92@gmail.com>
2023-09-11 17:56:35 +0300

    inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+this mmodule checks if the object is exactly an instance of the class
+"""
+
+
 def is_same_class(obj, a_class):
     """
     checks if the object is exactly an instance of the specified class
