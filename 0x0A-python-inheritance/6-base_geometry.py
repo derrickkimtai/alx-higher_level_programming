da69d8e244b8b51d79670c15d4ad8bac494d8aad
derrickkimtai <derrickmoio92@gmail.com>
2023-09-11 16:06:49 +0300

    exeception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """
+    it finds the area but it raises an execption
+    """
+    def area(self):
+        """
+        it was to calculate the area but it raises an exception
+        """
+        raise Exception("area() is not implemented")
