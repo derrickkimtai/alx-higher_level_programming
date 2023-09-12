abb5647a7a60416a1d48355b8979d941bc7058a8
derrickkimtai <derrickmoio92@gmail.com>
2023-09-12 16:56:03 +0300

    input

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this importation of json
+"""
+import json
+"""
+this is a module of json i python
+"""
+def from_json_string(my_str):
+    """
+    function that returns an object
+    """
+    return json.loads(my_str)
