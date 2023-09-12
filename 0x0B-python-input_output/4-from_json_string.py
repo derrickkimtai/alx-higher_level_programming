4132d3043bd69fc6b9af0755e101f4ef37caa652
derrickkimtai <derrickmoio92@gmail.com>
2023-09-12 17:23:31 +0300

    input

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -6,6 +6,8 @@ import json
 """
 this is a module of json i python
 """
+
+
 def from_json_string(my_str):
     """
     function that returns an object
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+this the importation of json module
+"""
+import json
+"""
+this the impelementing of json import
+"""
+def load_from_json_file(filename):
+    """
+    creates an object from a "JSON file"
+    """
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return data
