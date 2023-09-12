193075960cbe942bdda8eaa457781e07873e91ba
derrickkimtai <derrickmoio92@gmail.com>
2023-09-12 16:32:20 +0300

    input

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""
+this is a module about json
+"""
+def to_json_string(my_obj):
+    """
+    function that returns the json representation
+    """
+    return json.dumps(my_obj)
