76bb3d125eb7b9802b30d9230da1b8ab8a3ccb72
derrickkimtai <derrickmoio92@gmail.com>
2023-09-12 21:42:18 +0300

    input

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,28 +1,9 @@
 #!/usr/bin/python3
 """
-    this has no importation of a module in any case below
+defines a json class
 """
-
-
 def class_to_json(obj):
     """
-    returns the dictionary with simple data structure
+    functions of the dictionary data structure
     """
-    json_dict = {}
-
-    for attr_name in dir(obj):
-        if not attr_name.startswith("__"):
-            attr_value = getattr(obj, attr_name)
-
-            if isinstance(attr_value, (str, int, bool)):
-                json_dict[attr_name] = attr_value
-            elif isinstance(attr_value, (str, int, bool)):
-                serializable_items = [item for item in attr_value if isinstance(item, (str, int, bool))]
-                if serializable_items:
-                    json_dict[attr_name] = serializable_items
-            elif isinstance(attr_value, dict):
-                serializable_dict = {k: v for k, v in attr_value.items() if isinstance(v, (str, int, bool))}
-                if serializable_dict:
-                    json_dict[attr_name] = serializable_dict
-
-    return json_dict
+    return obj.__dict__
