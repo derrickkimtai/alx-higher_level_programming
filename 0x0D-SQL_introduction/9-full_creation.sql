afa84828acbdc25bb0dd3a2d63d714afad9653ac
derrickkimtai <derrickmoio92@gmail.com>
2023-10-17 12:49:36 -0700

    sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS second_table (
 	name VARCHAR(256)
 	SCORE INT
 );
-INSERT INTO second_table (id, name, score) VALUES
+
+INSERT INTO 'second_table' (id, name, score) VALUES
 	(1, 'John' 10),
 	(2, 'Alex' 3),
 	(3, 'Bob' 14),
