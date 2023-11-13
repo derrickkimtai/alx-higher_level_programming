169e7abe45995525941917e700b49af56dcf9c05
derrickkimtai <derrickmoio92@gmail.com>
2023-11-13 18:54:08 +0300

    javascript

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-function factorial(n) {
-    if (isNaN(n)) {
-        return 1;
-    } else if (n === 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
 const imput = parseInt(process.argv[2]);
 const result = factorial(imput);
