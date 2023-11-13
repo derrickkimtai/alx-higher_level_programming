c329f6b6b64ffdafb9af36776cb11d06c9ad9310
Kimtai Moiben <125466483+derrickkimtai@users.noreply.github.com>
2023-11-13 16:16:53 +0300

    javascript

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myArg = parseInt(process.argv[2]);
+if (!isNaN(myArg)) {
+    for (let i = 0; i <= myArg; i++); {
+        console.log('C is fun');
+    }
+} else {
+    console.log('Missing number of occurrences');
+}
