1ffa472035e3e0e732c95fb1f4c98866a2423f42
derrickkimtai <derrickmoio92@gmail.com>
2023-11-13 14:49:45 +0300

    javascritpt

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-const numArgs = process.argv.lenght;
+const numArgs = process.argv.length;
 
-if (numArgs === 2){
-    console.log("No argument");
-}
- else if (numArgs === 3){
-	 console.log("Argument found");
- }
-else{
-	console.log("Arguments found");
+if (numArgs === 2) {
+  console.log('No argument');
+} else if (numArgs === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
