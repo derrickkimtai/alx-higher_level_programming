1c0d268fb8464230dab0d8032a78027a9ce54cdd
derrickkimtai <derrickmoio92@gmail.com>
2023-11-14 19:47:30 +0300

    javascript

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,6 +10,6 @@ module.exports = class Rectangle {
   }
   print () {
       for (let i = 0; i < this.height; i++)
-      console.log('x'.repeat(this.width))
+      console.log('X'.repeat(this.width))
   }
 };
