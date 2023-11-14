5258dca0917c6786e28149c99c4e8ec9a2b0e815
derrickkimtai <derrickmoio92@gmail.com>
2023-11-14 23:16:17 +0300

    javascript

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,3 +1,12 @@
 #!/usr/bin/node
 const Square = require('./5-square.js');
-
+module.exports = class Square extends Square {
+	charprint (c) {
+		if (c === undefined) {
+			this.print();
+		} else {
+			for (let i = 0; i < this.height; i++)
+			{
+				console.log(c.repeat(this.width)); }
+	}
+};
