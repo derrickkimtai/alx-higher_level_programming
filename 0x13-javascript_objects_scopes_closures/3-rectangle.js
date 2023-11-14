c49a9c242bf577b832124821dd9bc516b4e11f6b
derrickkimtai <derrickmoio92@gmail.com>
2023-11-14 19:44:15 +0300

    javascrip

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,8 +5,7 @@ module.exports = class Rectangle {
       this.width = w;
       this.height = h;
     } else {
-      this.width = undefined;
-      this.height = undefined;
+      const emptyObject = {};
     }
   }
   print () {
