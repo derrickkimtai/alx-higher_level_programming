cbfdae002938455267e0445f6bdab2b296b87139
derrickkimtai <derrickmoio92@gmail.com>
2023-11-19 11:46:35 +0300

    sql

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,9 +13,9 @@ def get_state_cities(username, password, database, state_name):
     cursor = db.cursor()
 
     query = """
-        SELECT cities.id, cities.name
+        SELECT cities.id, cities.name, states.name
         FROM cities
-        JOIN states ON cities.state_id = states.id
+        INNER JOIN states ON cities.state_id = states.id
         WHERE states.name = %s
         ORDER BY cities.id ASC
     """
