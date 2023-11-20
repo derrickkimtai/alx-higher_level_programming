210d38ae8ce7111d387c080d9ade1403e3b270a1
derrickkimtai <derrickmoio92@gmail.com>
2023-11-20 01:05:25 -0800

    sql

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from sqlalchemy impor create_engine
-from sqlalchemy.orm import sessionmaker
+from SQLAlchemy impor create_engine
+from SQLAlchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
