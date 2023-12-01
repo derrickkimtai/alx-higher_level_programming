befc591435edb7a4f47438ce942206f540732097
derrickkimtai <derrickmoio92@gmail.com>
2023-12-01 16:46:06 +0300

    python network

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     with urllib.request.urlopen(url, timeout=10) as response:
-        request_id = response.headers['X-Request-Id']
+        request_id = response.headers.get('X-Request-Id')
         print(request_id)
