8ad177752ef9af9ad05a60051cd30e3b23701c30
derrickkimtai <derrickmoio92@gmail.com>
2023-12-01 06:20:23 -0800

    python network

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,5 +3,15 @@
      takes in a URL and an email, sends a POST request to the passed URL with the email
 """
 import sys
-import urllib
+import urllib.request
+import urllib.parse
 
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    with urllib.request.urlopen(url, data=data) as response:
+        body = response.read().decode('utf-8')
+        print(body)
