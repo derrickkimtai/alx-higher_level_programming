c741c54077570163a5eec941625cf6938d2b6161
derrickkimtai <derrickmoio92@gmail.com>
2023-12-05 01:27:50 -0800

    network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the body of the response
-url=$1
-
-response=$(curl -s -o /dev/null -w "%{http_code}" "url")
-
-if [ $response -eq 200 ]; then
-	body=$(curl -s $url)
-	echo "$body"
-else
-	echo "Error"
-fi
+curl -sL "$1"
