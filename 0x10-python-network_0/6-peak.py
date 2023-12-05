a64536d0c3a6b66d5a8f9a3ce07d4033a48b2ed1
derrickkimtai <derrickmoio92@gmail.com>
2023-12-05 02:17:50 -0800

    network

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+def find_peak(list_of_integers):
+    """
+        finds a peak in a list of unsorted integers.
+    """
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+
+    return list_of_integers[left]
+
